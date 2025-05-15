ba24930147c485cec0f23c69f9a7510d2b6beb9e
Tristan <trisdinning@gmail.com>
2025-05-15 11:12:25 +1000

    moved connection code to settings

--- settings.php
+++ settings.php
@@ -1,6 +1,9 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$sql_db = "cai_db";
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $sql_db = "cai_db";
+
+    $dbconn = @mysqli_connect($host, $user, $password, $sql_db);
+    if (!$dbconn) { die("Connection failed: " . mysqli_connect_error()); }
 ?>
--- test.php
+++ test.php
@@ -2,8 +2,6 @@
     // Just used to test database. Can be used as template.
     // Romove by end of project.
     require_once "settings.php";
-    $dbconn = @mysqli_connect($host, $user, $password, $sql_db);
-    if (!$dbconn) { die("Connection failed: " . mysqli_connect_error()); }
 
     $query = 'SELECT * FROM test_table';
     $result = mysqli_query($dbconn, $query);
